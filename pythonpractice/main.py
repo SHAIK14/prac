ea61178af71361d1a8604b64dd912d2e483c2ef3
shaik asif <smd.20sa@gmail.com>
2026-05-27 00:43:49 +0530

    revise

--- pythonpractice/main.py
+++ pythonpractice/main.py
@@ -1,1 +1,10 @@
-print("hello")
+class UPITransaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.status = "pending"
+
+
+paymentone = UPITransaction("asif", "rishit", 10000)
+paymenttwo = UPITransaction("asif", "riverindie", 2500)
