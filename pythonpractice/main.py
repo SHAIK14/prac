3aac14ad07578731d671cc1ff60cdfa8bb69040f
shaik asif <smd.20sa@gmail.com>
2026-05-27 01:46:42 +0530

    setter and getter

--- pythonpractice/main.py
+++ pythonpractice/main.py
@@ -2,9 +2,22 @@ class UPITransaction:
     def __init__(self, sender, receiver, amount):
         self.sender = sender
         self.receiver = receiver
-        self.amount = amount
+        self.__amount = amount
         self.status = "pending"
+
+    def show_details(self):
+        print(f"{self.sender} sent {self.__amount} to the {self.receiver}")
+
+    def get_amount(self):
+        return self.__amount
+
+    def set_amount(self, amount):
+        if amount > 0:
+            self.__amount = amount
 
 
 paymentone = UPITransaction("asif", "rishit", 10000)
 paymenttwo = UPITransaction("asif", "riverindie", 2500)
+print(paymentone.get_amount())
+print(paymentone.set_amount(1000))
+print(paymentone.get_amount())
