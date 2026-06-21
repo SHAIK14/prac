3e066a4907a5b76b83de5fc9f1b88a719fe1fc92
shaik asif <smd.20sa@gmail.com>
2026-06-21 20:14:09 +0530

    game

--- connect4/main.py
+++ connect4/main.py
@@ -21,7 +21,7 @@ class Board:
         for dr, dc in directions:
             total = 1
             total += self.count_dir(row, col, dr, dc, symbol)
-            total += self.count_dir(row, col, dr, dc, symbol)
+            total += self.count_dir(row, col, -dr, -dc, symbol)
             if total >= 4:
                 return True
 
@@ -36,3 +36,38 @@ class Board:
             r += dr
             c += dc
         return total
+
+    def check_draw(self):
+        for c in range(7):
+            if self.board[0][c] == "":
+                return False
+        return True
+
+
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.players = [Player("P1", "X"), Player("P2", "O")]
+        self.current = 0
+
+    def switch_player(self):
+        self.current = 1 - self.current
+
+    def play_move(self, col):
+        current_player = self.players[self.current]
+        sym = current_player.symbol
+
+        if self.board.is_column_full(col):
+            return "col full"
+        row = self.board.place_disc(col, sym)
+        if self.board.check_win(row, col, sym):
+            return f" winner is {sym}"
+        if self.board.check_draw():
+            return "draw match"
+        self.switch_player()
